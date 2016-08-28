455868ef088dfca71e6718fa8f4ebd06980a10eb
richard12511 <richardnew12511@gmail.com>
2016-08-27 19:06:47 -0500

    list diff

--- app/Modules.hs
+++ app/Modules.hs
@@ -27,4 +27,6 @@ where
   elem' :: (Eq a) => a -> [a] -> Bool
   elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
 
+  listDiff = [1..10] \\ [2,3]
+
   partitionedList = partition (`elem` ['A'..'Z']) "hellO There everYbody"
